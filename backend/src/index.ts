98fefc4e5ba804b5fa6e7b7e84d21075a6cd7ee5
Shernan Javier <shernanjavier@gmail.com>
2024-09-19 15:23:41 -0400

    Fixed errors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,18 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import authRoutes from './routes/authRoutes';
 import mongoose from 'mongoose';
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('API is running...');
-});
+app.use('/api/auth', authRoutes);
 
-mongoose.connect(process.env.MONGO_URI || '')
+
+mongoose.connect(process.env.MONGODB_URI || '')
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
